4fbc9a908eba109b3852773e63f5ec795f14408a
MatsunagaNaoya <mh8scjf6f76ns49r71fb@gmail.com>
2025-03-25 21:50:58 +0900

    Postgresql接続確認

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, TypeScript!");
+import pool from "./db";
+
+async function testDB() {
+  try {
+    const res = await pool.query("SELECT NOW()");
+    console.log("Database connected! Current time:", res.rows[0].now);
+  } catch (error) {
+    console.error("Database connection error:", error);
+  } finally {
+    pool.end();
+  }
+}
+
+testDB();
